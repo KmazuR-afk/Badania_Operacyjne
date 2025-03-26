fec202a431b0076b00085366a3b075399a1a6739
Kacper Mazur <tasnko12@gmail.com>
2025-03-26 16:58:02 +0100

    laby 26.03.2025

--- source/main.cpp
+++ source/main.cpp
@@ -2,44 +2,48 @@
 #include "../include/Graf.h"
 
 int main() {
-    Graf g1(6);
-    std::srand(std::time(0));
-    g1.addEdge(0, 1, 3);
-g1.addEdge(0, 4, 3);
-g1.addEdge(0, 5, 6);
-
-g1.addEdge(1, 2, 1);
-g1.addEdge(1, 3, 3);
-
-g1.addEdge(2, 3, 3);
-g1.addEdge(2, 5, 1);
-
-g1.addEdge(5, 3, 1);
-g1.addEdge(4, 5, 2);
-    /*auto randomWeight = []() { return std::rand() % 10 + 1; };
-    g1.addEdge(0, 1, randomWeight());
-    g1.addEdge(1, 2, randomWeight());
-    g1.addEdge(2, 3, randomWeight());
-    g1.addEdge(3, 0, randomWeight());
-    g1.addEdge(0, 2, randomWeight());
-    g1.addEdge(1, 3, randomWeight());
-    g1.addEdge(0, 4, randomWeight());
-    g1.addEdge(4, 2, randomWeight());
-    g1.addEdge(1, 4, randomWeight());
-    g1.addEdge(3, 4, randomWeight());
-    g1.addEdge(4, 5, randomWeight());
-    g1.addEdge(3, 7, randomWeight());
-    g1.addEdge(5, 6, randomWeight());
-    g1.addEdge(6, 7, randomWeight());
-    g1.addEdge(7, 8, randomWeight());
-    g1.addEdge(8, 5, randomWeight()); // Tworzy cykl 5-6-7-8-5
-    g1.addEdge(5, 7, randomWeight());
-    g1.addEdge(6, 8, randomWeight());
-    g1.addEdge(5, 9, randomWeight());
-    g1.addEdge(6, 9, randomWeight());
-    g1.addEdge(7, 9, randomWeight());
-    g1.addEdge(8, 9, randomWeight());*/
-    g1.display(3);
+    Graf g(6);
+    g.addEdge(0, 1, 5);
+    g.addEdge(0, 2, 7);
+    g.addEdge(0, 3, 3);
+    g.addEdge(0, 4, 2);
+    g.addEdge(0, 5, 8);
+    g.addEdge(0, 6, 4);
+    
+    g.addEdge(1, 2, 6);
+    g.addEdge(1, 3, 1);
+    g.addEdge(1, 7, 7);
+    g.addEdge(1, 8, 2);
+    
+    g.addEdge(2, 3, 3);
+    g.addEdge(2, 6, 5);
+    g.addEdge(2, 9, 8);
+    
+    g.addEdge(3, 4, 4);
+    g.addEdge(3, 7, 6);
+    
+    g.addEdge(4, 5, 1);
+    g.addEdge(4, 8, 5);
+    
+    g.addEdge(5, 6, 9);
+    g.addEdge(5, 9, 3);
+    
+    g.addEdge(6, 7, 2);
+    g.addEdge(6, 8, 6);
+    
+    g.addEdge(7, 8, 4);
+    g.addEdge(7, 9, 7);
+    
+    g.addEdge(8, 9, 2);
+    
+    g.addEdge(1, 9, 3);
+    g.addEdge(0, 9, 4);
+    g.addEdge(4, 6, 5);
+    g.addEdge(3, 5, 6);
+    g.addEdge(2, 8, 7);
+    
+    /*auto randomWeight = []() { return std::rand() % 10 + 1; };*/
+    g.display(3);
     /*std::vector<int> bfs1=g1.BFS(3);
     std::cout<<"BFS:\n";
     for(auto i:bfs1){
@@ -57,12 +61,13 @@ g1.addEdge(4, 5, 2);
         for(int j=0;j<6;j++){
             std::cout<<"poprzednik "<<d[0][j]<<" waga "<<d[1][j]<<std::endl;
         }*/
-    std::vector<int> heur=g1.countHeuristicDijkstra(3);
+    std::vector<int> heur=g.countHeuristicDijkstra(7);
+    std::cout<<"heurystyka:"<<std::endl;
     for(int u:heur){
         std::cout<<u<<" - ";
     }
     std::cout<<std::endl;
-    std::vector<std::vector<int>> ast=g1.Astar(0,3,heur);
+    std::vector<std::vector<int>> ast=g.Astar(0,7,heur);
     for(int i=0;i<(int)ast[0].size();i++)
     {
         std::cout<<"poprzednik "<<ast[0][i]<<" aktualny koszt "<<ast[1][i]<<std::endl;
